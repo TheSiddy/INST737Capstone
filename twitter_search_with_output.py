6c0943daed016ce1695d68244e6f51809d131306
TheSiddy <32276561+TheSiddy@users.noreply.github.com>
2019-03-20 15:55:55 -0400

    Filter by language and location

--- twitter_search_with_output.py
+++ twitter_search_with_output.py
@@ -17,11 +17,12 @@ from twitter import *
 
 import sys
 import csv
-
-latitude = 51.474144    # geographical centre of search
-longitude = -0.035401    # geographical centre of search
-max_range = 1             # search range in kilometres
-num_results = 50        # minimum results to obtain
+latitude = 48.8566
+longitude = 2.3522
+#latitude = 51.474144    # geographical centre of search
+#longitude = -0.035401    # geographical centre of search
+max_range = 10             # search range in kilometres
+num_results = 100        # minimum results to obtain
 outfile = "output.csv"
 
 #-----------------------------------------------------------------------
@@ -47,7 +48,7 @@ csvwriter = csv.writer(csvfile)
 #-----------------------------------------------------------------------
 # add headings to our CSV file
 #-----------------------------------------------------------------------
-row = [ "user", "text", "latitude", "longitude" ]
+row = [ "user", "text", "latitude", "longitude", "location", "language" ]
 csvwriter.writerow(row)
 
 #-----------------------------------------------------------------------
@@ -62,24 +63,26 @@ while result_count <  num_results:
     # perform a search based on latitude and longitude
     # twitter API docs: https://dev.twitter.com/rest/reference/get/search/tweets
     #-----------------------------------------------------------------------
-    query = twitter.search.tweets(q = "", geocode = "%f,%f,%dkm" % (latitude, longitude, max_range), count = 100, max_id = last_id)
+    query = twitter.search.tweets(q = "", geocode = "%f,%f,%dkm" % (latitude, longitude, max_range), lang = "fr", count = 100, max_id = last_id)
     print(query, '\n\t')
 
     for result in query["statuses"]:
         #-----------------------------------------------------------------------
         # only process a result if it has a geolocation
         #-----------------------------------------------------------------------
-        if result["geo"]:
+        if result["lang"]=='en':
             user = result["user"]["screen_name"]
             text = result["text"]
             text = text.encode('ascii', 'replace')
-            latitude = result["geo"]["coordinates"][0]
-            longitude = result["geo"]["coordinates"][1]
+#            latitude = result["geo"]["coordinates"][0]
+#            longitude = result["geo"]["coordinates"][1]
+            location = result["user"]["location"]
+            language = result["lang"]
 
             #-----------------------------------------------------------------------
             # now write this row to our CSV file
             #-----------------------------------------------------------------------
-            row = [ user, text, latitude, longitude ]
+            row = [ user, text, latitude, longitude, location, language ]
             csvwriter.writerow(row)
             result_count += 1
         last_id = result["id"]
